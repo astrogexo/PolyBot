77c26b88a74aa432405da289da5a0bb56f23ab82
astrogexo <32209658+astrogexo@users.noreply.github.com>
2021-10-18 01:12:38 -0400

    Add module to coin functions

---
+++ coins/test.js
@@ -0,0 +1,37 @@
+coins = {
+    matic:  (require('./Matic')).getPrice,
+    eth:  (require('./Eth')).getPrice,
+    wexpoly:  (require('./WexPoly')).getPrice,
+    titan:  (require('./Titan')).getPrice,
+    bunny:  (require('./Bunny')).getPrice,
+    dino:  (require('./Dino')).getPrice,
+    addy:  (require('./Addy')).getPrice,
+    pspace:  (require('./PSpace')).getPrice,
+    pup:  (require('./Pup')).getPrice,
+    bone:  (require('./Bone')).getPrice,
+    dolph:  (require('./Dolph')).getPrice,
+    qi:  (require('./Qi')).getPrice,
+    dojo:  (require('./Dojo')).getPrice,
+    ice:  (require('./Ice')).getPrice,
+    pwing:  (require('./Pwing')).getPrice,
+    raider:  (require('./Raider')).getPrice,
+    movr:  (require('./Movr')).getPrice,
+    ivory:  (require('./Ivory')).getPrice,
+    ftm:  (require('./Ftm')).getPrice,
+    tower:  (require('./Tower')).getPrice,
+    bnb:  (require('./Bnb')).getPrice,
+    cake:  (require('./Cake')).getPrice,
+    monsta:  (require('./Monsta')).getPrice,
+    gyro:  (require('./Gyro')).getPrice,
+    aurum:  (require('./Aurum')).getPrice,
+    oasis:  (require('./Oasis')).getPrice,
+    klima:  (require('./Klima')).getPrice,
+    bct:  (require('./Bct')).getPrice,
+    avax:  (require('./Avax')).getPrice,
+    time:  (require('./Time')).getPrice,
+}
+
+module.exports = coins
+
+// aklima:  (require('../coins/aklima')).getPrice,
+
